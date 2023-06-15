a4f4f21004c2eb87a53232e3e56bacced4ee329c
Anthony Miranda <mirandaj20@students.ecu.edu>
2023-06-15 13:17:46 -0400

    Adding Projects Space Template

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import { Tilt } from 'react-tilt';
+import { motion } from 'framer-motion';
 
-const Works = () => {
-  return (
-    <div>Works</div>
+import { styles } from '../styles';
+import { github } from '../assets';
+import { SectionWrapper } from '../hoc';
+import { projects } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion';
+
+const ProjectCard = ({ index, name, description, tags, image, source_code_link}) => {
+  return(
+    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+      <Tilt
+        option={{
+          max:45,
+          scale:1,
+          speed:450
+        }}
+        className="bg-tertiary p-5 rounded-2x1 sm:w-[360px] w-full"
+      >
+        <div className="relative w-full h-[230px]">
+          <img 
+            src={image}
+            alt={name}
+            className="w-full h-full object-cover rounded-2xl"
+          />
+        </div>
+
+      </Tilt>
+    </motion.div>
   )
 }
 
-export default Works
+const Works = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>My Work</p>
+        <h2 className={styles.sectionHeadText}>Projects.</h2>
+      </motion.div>
+      <div className="w-full flex">
+        <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+          className="mt-3 text-secondary text-[17px] max-w-3x1 leading-[30px]"        
+        >
+          Here are some of my projects. Each project has a brief description with a link
+          to code repositories. A reflection of my ability to solve complex problems, work
+          with a broad tech stack, and collaborate with others."
+        </motion.p>
+      </div>
+
+      <div className="mt-20 flex flex-wrap gap-7">
+        {projects.map((project, index) => (
+          <ProjectCard key={`project-${index}`}
+          index={index}
+          {...project}
+          />
+        ))}
+      </div>
+
+    </>
+  )
+}
+
+export default SectionWrapper(Works, "");

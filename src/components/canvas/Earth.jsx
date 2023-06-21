963f1895e4e4904a58b035535d8036c6a9950341
Anthony Miranda <mirandaj20@students.ecu.edu>
2023-06-21 12:42:45 -0400

    Added Globe render and stars

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -8,7 +8,12 @@ const Earth = () => {
   const earth = useGLTF("./planet/scene.gltf");
 
   return (
-    <primitive object={earth.scene} />
+    <primitive 
+      object={earth.scene} 
+      scale={2.5}
+      position-y={0}
+      position-x={0}
+    />
   );
 };
 
